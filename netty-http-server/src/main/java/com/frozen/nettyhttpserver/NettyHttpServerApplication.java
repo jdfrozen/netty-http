78d0ea270967332d6490f1720c04299a464d281d
jdfrozen <616769416@qq.com>
2018-12-29 16:20:44 +0800

    新建netty http

---
+++ netty-http-server/src/main/java/com/frozen/nettyhttpserver/NettyHttpServerApplication.java
@@ -0,0 +1,14 @@
+package com.frozen.nettyhttpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyHttpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyHttpServerApplication.class, args);
+    }
+
+}
+
